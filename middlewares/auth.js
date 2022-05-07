6a0a84df1cf251b7900a89716cd834b6fe70ba9c
marja555 <marja555@yandex.ru>
2022-05-07 18:22:21 +0500

    Отловить ош через центральный обработчик

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,16 +4,16 @@ const AuthorizationError = require('../errors/AuthorizationError');
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    return next(new AuthorizationError('Ошибка авторизации'));
+    next(new AuthorizationError('Ошибка авторизации'));
   }
 
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-word');
   } catch (err) {
-    return next(new AuthorizationError('Необходима авторизация'));
+    next(new AuthorizationError('Необходима авторизация'));
   }
   req.user = payload;
 
-  return next();
+  next();
 };
